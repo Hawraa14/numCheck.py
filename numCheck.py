9945dfa008d5dd8573f85ea77444ed3dfe949701
Hawraa14 <94127336+Hawraa14@users.noreply.github.com>
2021-12-12 16:45:21 +0400

    Add files via upload

---
+++ numCheck.py
@@ -0,0 +1,8 @@
+Python 3.10.0 (tags/v3.10.0:b494f59, Oct  4 2021, 19:00:18) [MSC v.1929 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+a=30
+b=20
+if a==b:
+  print('a is equal to b')
+else:
+  print('a and b are not equal')
